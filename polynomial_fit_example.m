6999b89dfbae8264c387740137d96150fc4176fd
oturgut <ozhanturgut@hotmail.com>
2018-09-05 23:36:43 -0700

    Polynomial Fit Comment Update

--- polynomial_fit_example.m
+++ polynomial_fit_example.m
@@ -9,7 +9,7 @@ fitpoly2 = fit(cdate, pop, 'poly2');
 plot(fitpoly2, cdate, pop)
 legend('Location','Best')
 
-
+% Fit options
 fitpoly3 = fit(cdate, pop, 'poly3', 'Normalize','on','Robust','on')
 figure
 plot(fitpoly3, cdate, pop)
@@ -18,6 +18,7 @@ fitoptions poly3
 
 [fitpoly3, gof] = fit(cdate, pop, 'poly3', 'Normalize','on','Robust','on')
 
+% Beyond the data range
 figure
 plot(cdate, pop,'o')
 xlim([1900, 2050]);
@@ -25,5 +26,6 @@ hold on
 plot(fitpoly3)
 hold off
 
+% Plot confidence bounds
 figure
 plot(fitpoly3, cdate, pop, 'predobs')
